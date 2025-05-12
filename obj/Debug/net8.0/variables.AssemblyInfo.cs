0f37b290f0fc92c444cf40a64d8f97408b2b0ac2
davidlandau770 <dal778770@gmail.com>
2025-05-12 12:49:37 +0300

    add git ignore

--- obj/Debug/net8.0/variables.AssemblyInfo.cs
+++ obj/Debug/net8.0/variables.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83493b9083f65351c6700c37856063622696022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cf834a50c83839c51a00040c3a10bd84fc8511")]
 [assembly: System.Reflection.AssemblyProductAttribute("variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
